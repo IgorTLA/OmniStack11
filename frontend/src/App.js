9d8eebbf5fed1a5eb461aed4aa1a7dee10efb881
IgorTLA <igort.lira@gmail.com>
2022-04-02 17:27:39 -0300

    App Be the Hero

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// JSX (JavaScript XML)
+import './global.css';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
